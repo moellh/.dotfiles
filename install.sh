55e5c02ea9fbe76b376637f941ac59c35ced56cf
Henrik Möllmann <henmoell@gmail.com>
2025-05-29 16:56:59 +0200

    Removed tmux plugins from install

--- install.sh
+++ install.sh
@@ -108,19 +108,15 @@ git config --global user.email "$git_email"
 
 # ------------------------------------------------------------------------------
 
-# installs zsh plugins
+# Install zsh plugins
 mkdir -p ~/.zsh
 git clone https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 
-# installs tmux plugins
+# Install Tmux plugin manager
 mkdir -p ~/.config/tmux/plugins
-git clone https://github.com/dreamsofcode-io/catppuccin-tmux.git ~/.config/tmux/plugins/catppuccin-tmux
-git clone https://github.com/tmux-plugins/tmux-sensible.git ~/.config/tmux/plugins/tmux-sensible
-git clone https://github.com/tmux-plugins/tmux-yank.git ~/.config/tmux/plugins/tmux-yank
 git clone https://github.com/tmux-plugins/tpm.git ~/.config/tmux/plugins/tpm
-git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.config/tmux/plugins/vim-tmux-navigator
 
 # nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
