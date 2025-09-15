0a48052e927be01299757261264b49694136758a
Henrik Möllmann <henmoell@gmail.com>
2025-09-15 17:42:41 +0200

    feat(nvim): disable snack.nvim image feature
    Maybe only enable for very few features in the future

--- config/.config/nvim/lua/plugins/snacks.lua
+++ config/.config/nvim/lua/plugins/snacks.lua
@@ -1,11 +1,13 @@
 return {
-  "folke/snacks.nvim",
-  ---@type snacks.Config
-  opts = {
-    image = {
-      -- your image configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  }
+  -- "folke/snacks.nvim",
+  -- enable = false,
+  --
+  -- ---@type snacks.Config
+  -- opts = {
+  --   image = {
+  --     -- your image configuration comes here
+  --     -- or leave it empty to use the default settings
+  --     -- refer to the configuration section below
+  --   }
+  -- }
 }
