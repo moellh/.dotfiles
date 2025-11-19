fcc25e4f03ffa7e93b9a2f8784aca35127bfb467
Henrik Möllmann <henmoell@gmail.com>
2025-11-19 11:06:05 +0100

    feat(nvim)!: remove ignored_filetypes in treesitter, add initial file mappings

--- config/.config/nvim/lua/plugins/treesitter.lua
+++ config/.config/nvim/lua/plugins/treesitter.lua
@@ -10,60 +10,31 @@ return {
             -- Install all parsers
             require("nvim-treesitter").install "unstable"
 
-            -- Skip buffers with some filetypes
-            local ignored_filetypes = {
-                -- Neovim
-                "", -- no filetype
-                "help", -- help page
-                "qf", -- quickfix list
-
-                -- generic file types
-                "conf",
-
-                -- Telescope
-                "TelescopePrompt",
-                "TelescopeResults",
-
-                -- Avante
-                "AvantePromptInput",
-                "AvanteInput",
-                "AvanteSelectedFiles",
-                "AvanteSelectedCode",
-                "AvanteTodos",
-
-                -- Oil
-                "oil",
-                "oil_preview",
-
-                -- CMP
-                "cmp_menu",
-                "cmp_docs",
-
-                -- lazy
-                "lazy",
-                "lazy_backdrop",
-
-                -- Other plugins
-                "aerial",
-                "undotree",
-            }
-
             -- Enable highlighting
             vim.api.nvim_create_autocmd("FileType", {
                 pattern = "*",
                 callback = function()
                     local buf = vim.api.nvim_get_current_buf()
                     local ft = vim.bo[buf].filetype
-                    if vim.tbl_contains(ignored_filetypes, ft) then
+                    local filename = vim.api.nvim_buf_get_name(buf)
+
+                    local mappings = {
+                        ["hypr*.conf"] = "hyprlang",
+                    }
+
+                    for pattern, lang in pairs(mappings) do
+                        if string.match(filename, pattern) then
+                            vim.treesitter.start(buf, lang)
+                            return
+                        end
+                    end
+
+                    if vim.tbl_contains(require("nvim-treesitter").get_installed("parsers"), ft) then
+                        vim.treesitter.start(buf)
                         return
                     end
 
-                    if ft == "text" then
-                        vim.treesitter.start(buf, "markdown")
-                        return
-                    end
-
-                    vim.treesitter.start(buf)
+                    -- Else ignore
                 end,
             })
 
